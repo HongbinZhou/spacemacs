763ad293fc98a6644f5022ce83c9156683a7abb9
Hongbin Zhou <hongbinzhou@hotmail.com>
2016-01-20 14:12:14 +0800

    add package recursive-narrow and narrow-indirect.

--- lisp/my-init.el
+++ lisp/my-init.el
@@ -84,3 +84,8 @@
 ;; added keybindings
 (spacemacs/set-leader-keys
   "br" 'rename-buffer)
+
+;; narrow-indirect
+(define-key ctl-x-4-map "nd" 'ni-narrow-to-defun-indirect-other-window)
+(define-key ctl-x-4-map "nn" 'ni-narrow-to-region-indirect-other-window)
+(define-key ctl-x-4-map "np" 'ni-narrow-to-page-indirect-other-window)
