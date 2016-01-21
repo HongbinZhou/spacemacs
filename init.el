6712e6b2a9269dab583caa304e5586c8d26cb7bb
Hongbin Zhou <hongbinzhou@hotmail.com>
2016-01-21 18:36:35 +0800

    set *scratch* default mode to emacs-lisp-mode.
    https://github.com/TheBB/spacemacs/commit/643fb7904e0489c035881faad5bb4ec35a807a1f

--- init.el
+++ init.el
@@ -107,6 +107,8 @@ values."
    ;; Possible values are: `recents' `bookmarks' `projects'.
    ;; (default '(recents projects))
    dotspacemacs-startup-lists '(recents projects)
+   ;; Default major mode of the scratch buffer (default `text-mode')
+   dotspacemacs-scratch-mode 'emacs-lisp-mode
    ;; Number of recent files to show in the startup buffer. Ignored if
    ;; `dotspacemacs-startup-lists' doesn't include `recents'. (default 5)
    dotspacemacs-startup-recent-list-size 5
