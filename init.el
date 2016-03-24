efd22dd5bc4f502a09f2baf3bdbb979a9f41b20a
Hongbin Zhou <hongbinzhou@hotmail.com>
2016-03-24 16:23:06 +0800

    Don't overwrite my C-z!

--- init.el
+++ init.el
@@ -305,3 +305,9 @@ layers configuration. You are free to put any user code."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-toggle-key ""))
