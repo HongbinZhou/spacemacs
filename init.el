5338451282d3cc132bd61fad7fc231f6b0ca62de
Hongbin Zhou <hongbinzhou@hotmail.com>
2017-05-24 23:05:12 +0800

    turn off wakatime as it broken on mac.

--- init.el
+++ init.el
@@ -84,7 +84,7 @@ values."
      ;;; my private layers
      my-dired
      my-shell
-     my-wakatime
+     ;; my-wakatime
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
