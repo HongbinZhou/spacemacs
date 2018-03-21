edf24f6c78f9074bedd9d6adf9a4629c1347c0a9
Hongbin Zhou <hongbinzhou@hotmail.com>
2018-03-21 10:55:31 +0800

    update fresh setup.

--- bin/init.sh
+++ bin/init.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# install cask
-if ! type cask > /dev/null
-then
-    curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python2
-fi
-
 whereAmI=$(dirname $(readlink -f $0))
 
-# link .emacs
-[[ ! -s ~/.emacs ]] && ln -s $whereAmI/../lisp/dotemacs ~/.emacs
-
-[[ ! -d ~/.emacs.d ]] && echo "Error: ~/.emacs.d not exist! Please rename the cloned repo to .emacs.d"
-
+[[ -d ~/.emacs.d ]] && mv ~/.emacs.d ~/.emacs.d.bak
 (
-    cd ~/.emacs.d
-    git submodule init
-    git submodule update
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    cd ~/.emacs.d && git checkout develop
 )
+
